55f3a7c3965840136e9bd406109dac2838ab34d1
motoish <yuan.zhang@gmx.com>
2024-09-27 18:05:44 +0900

    chore: optimize with shell

---
+++ bin/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BIN_PATH="$(dirname -- ${0})"
+DIST_PATH="dist"
+DOCUMENT_DIR="documents"
+ASSET_DIR="$DOCUMENT_DIR/assets"
+DOC_DIR="$DOCUMENT_DIR/docs"
+
+rm -rf $DIST_PATH
+
+# swaggerの設定
+cp "$DOC_DIR/blog-api/afs-client.yaml" $DIST_PATH/blog-api/
+perl -pe 's/<ymlPath>/afs-client.yaml/g' "$ASSET_DIR/swagger-ui.html" > $DIST_PATH/blog-api/api.html
